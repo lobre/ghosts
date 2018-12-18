5943140ab9f8224b20444f94e287c7d8c5e0e79a
Loric Brevet <loric.brevet@gmail.com>
2018-12-18 15:06:24 +0100

    Hosts cleaning on add, css pointer on link and removed force crlf processing

--- hosts.go
+++ hosts.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
-
 	"github.com/lobre/goodhosts"
 )
 
@@ -51,21 +48,14 @@ func (h hostsProcessor) add(ids ...string) error {
 
 		for _, segment := range entry.Segments {
 			for _, host := range segment.Hosts {
-				if !hosts.Has(ip, host) {
-					hosts.Add(ip, host)
-				}
+				hosts.Remove("", host)
+				hosts.Add(ip, host)
 			}
 		}
 	}
 
-	if err := hosts.Flush(); err != nil {
+	if err := hosts.Flush(h.config.HostsForceCRLF); err != nil {
 		return err
-	}
-
-	if h.config.HostsForceCRLF {
-		if err := forceCrlfEOL(); err != nil {
-			return err
-		}
 	}
 
 	return nil
@@ -104,14 +94,8 @@ func (h hostsProcessor) remove(ids ...string) error {
 		}
 	}
 
-	if err := hosts.Flush(); err != nil {
+	if err := hosts.Flush(h.config.HostsForceCRLF); err != nil {
 		return err
-	}
-
-	if h.config.HostsForceCRLF {
-		if err := forceCrlfEOL(); err != nil {
-			return err
-		}
 	}
 
 	return nil
@@ -135,27 +119,4 @@ func (h hostsProcessor) isUnique(host string, entry entry) (bool, error) {
 		}
 	}
 	return true, nil
-}
-
-// To be shifted the the goodhosts project
-func forceCrlfEOL() error {
-	hosts, err := goodhosts.NewHosts()
-	if err != nil {
-		return err
-	}
-
-	read, err := ioutil.ReadFile(hosts.Path)
-	if err != nil {
-		return err
-	}
-
-	// Replace lf by crlf
-	normalized := bytes.Replace(read, []byte{10}, []byte{13, 10}, -1)
-
-	err = ioutil.WriteFile(hosts.Path, normalized, 0)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
